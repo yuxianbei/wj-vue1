daec98e03a4477d16015b4c0d6b8ac571dd1eb2d
yuxianbei <1330105580@qq.com>
2022-06-30 20:23:12 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<!--  和index.html里面id="app"的没有关系,他只是对应下面的样式-->
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+<script>
+
+  import NavMenu from '@/components/common/NavMenu'
+// export default 意思是将这个组件整体导出，之后就可以使用import导入组件
+//   大括号里面是这个组件的相关属性
+  export default {
+    name: 'App',
+    components: {NavMenu}
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /*margin-top: 10px;*/
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,236 @@
+// 其中 vue 模块在 node_modules 中，
+import Vue from 'vue'
+// App 即 App.vue 里定义的组件
+import App from './App'
+// router 即 router 文件夹里定义的路由。
+import router from './router'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import 'echarts/theme/macarons.js'
+import store from './store'
+
+import {
+  Pagination,
+  Dialog,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Input,
+  Checkbox,
+  CheckboxButton,
+  CheckboxGroup,
+  Switch,
+  Select,
+  Option,
+  Button,
+  ButtonGroup,
+  Table,
+  TableColumn,
+  Tooltip,
+  Breadcrumb,
+  BreadcrumbItem,
+  Form,
+  FormItem,
+  Tabs,
+  TabPane,
+  Tag,
+  Tree,
+  Alert,
+  Icon,
+  Row,
+  Col,
+  Upload,
+  Progress,
+  Spinner,
+  Badge,
+  Card,
+  Rate,
+  Steps,
+  Step,
+  Carousel,
+  CarouselItem,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
+  Timeline,
+  TimelineItem,
+  Link,
+  Divider,
+  Image,
+  Loading,
+  MessageBox,
+  Message,
+  Notification
+} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Input)
+Vue.use(Checkbox)
+Vue.use(CheckboxButton)
+Vue.use(CheckboxGroup)
+Vue.use(Switch)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Button)
+Vue.use(ButtonGroup)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Tooltip)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Tabs)
+Vue.use(TabPane)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Alert)
+Vue.use(Icon)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Upload)
+Vue.use(Progress)
+Vue.use(Spinner)
+Vue.use(Badge)
+Vue.use(Card)
+Vue.use(Rate)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
+Vue.use(Link)
+Vue.use(Divider)
+Vue.use(Image)
+
+Vue.use(Loading.directive)
+
+Vue.prototype.$loading = Loading.service
+Vue.prototype.$msgbox = MessageBox
+Vue.prototype.$alert = MessageBox.alert
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$prompt = MessageBox.prompt
+Vue.prototype.$notify = Notification
+Vue.prototype.$message = Message
+
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8443/api'
+// 使请求带上凭证信息
+axios.defaults.withCredentials = true
+
+Vue.prototype.$axios = axios
+// 作用是阻止vue 在启动时生成生产提示。
+Vue.config.productionTip = false
+Vue.use(mavonEditor)
+
+router.beforeEach((to, from, next) => {
+    if (store.state.username && to.path.startsWith('/admin')) {
+      initAdminMenu(router, store)
+    }
+    if (store.state.username && to.path.startsWith('/login')) {
+      next({
+        name: 'Dashboard'
+      })
+    }
+    // 如果前端没有登录信息则直接拦截，如果有则判断后端是否正常登录（防止构造参数绕过）
+    if (to.meta.requireAuth) {
+      if (store.state.username) {
+        axios.get('/authentication').then(resp => {
+          if (resp) {
+            next()
+          }
+        })
+      } else {
+        next({
+          path: 'login',
+          query: {redirect: to.fullPath}
+        })
+      }
+    } else {
+      next()
+    }
+  }
+)
+
+// http response 拦截器
+axios.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    if (error) {
+      store.commit('logout')
+      router.replace('/login')
+    }
+    // 返回接口返回的错误信息
+    return Promise.reject(error)
+  })
+
+const initAdminMenu = (router, store) => {
+  // 防止重复触发加载菜单操作
+  if (store.state.adminMenus.length > 0) {
+    return
+  }
+  axios.get('/menu').then(resp => {
+    if (resp && resp.status === 200) {
+      var fmtRoutes = formatRoutes(resp.data.result)
+      router.addRoutes(fmtRoutes)
+      store.commit('initAdminMenu', fmtRoutes)
+    }
+  })
+}
+
+const formatRoutes = (routes) => {
+  let fmtRoutes = []
+  routes.forEach(route => {
+    if (route.children) {
+      route.children = formatRoutes(route.children)
+    }
+
+    let fmtRoute = {
+      path: route.path,
+      component: resolve => {
+        require(['./components/admin/' + route.component + '.vue'], resolve)
+      },
+      name: route.name,
+      nameZh: route.nameZh,
+      iconCls: route.iconCls,
+      meta: {
+        requireAuth: true
+      },
+      children: route.children
+    }
+    fmtRoutes.push(fmtRoute)
+  })
+  return fmtRoutes
+}
+
+/* eslint-disable no-new */
+// 创建了一个 Vue 对象（实例）
+new Vue({
+  // el 属性提供一个在页面上已存在的 DOM 元素作为 Vue 对象的挂载目标
+  el: '#app',
+  render: h => h(App),
+  // router 代表该对象包含 Vue Router，并使用项目中定义的路由。
+  router,
+  store,
+  // components 表示该对象包含的 Vue 组件，
+  components: {App},
+  // template 是用一个字符串模板作为 Vue 实例的标识使用，类似于定义一个 html 标签
+  template: '<App/>'
+})
